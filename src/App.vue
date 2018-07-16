e6a1db4e1f5316376907dd4413f62fe6fd4f1543
flyweiing <flyweiing@163.com>
2018-07-16 23:48:29 +0800

    tab

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <template>
   <div id="app">
     <m-header></m-header>
+    <!--<tab></tab>-->
     <router-view></router-view>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
 import MHeader from 'components/m-header/m-header'
+//import Tab from 'components/m-header/m-header'
 
 export default {
   name: 'App',
