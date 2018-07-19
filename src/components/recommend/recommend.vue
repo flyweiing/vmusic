804eb8c661bd69a92cb6b8ee43e120f14e1778ec
flyweiing <flyweiing@163.com>
2018-07-20 00:47:59 +0800

    recommend

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -1,15 +1,50 @@
 <template>
   <div class="recommend" ref="recommend">
-    <div class="recommend-list">
-      <h1 class="list-title">热门歌单推荐</h1>
+    <div class="recommend-content">
+      <div v-if="recommends.length" class="slider-wrapper" ref="sliderWrapper">
+        <slider>
+          <div v-for="item in recommends">
+            <a :href="item.linkUrl">
+              <img class="needslick" :src="item.picUrl">
+            </a>
+          </div>
+        </slider>
+      </div>
+      <div class="recommend-list">
+        <h1 class="list-title">热门歌单推荐</h1>
+        <ul></ul>
+      </div>
     </div>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
+import Slider from 'base/slider/slider'
+import { getRecommend } from 'api/recommend'
+import { ERR_OK } from 'api/config'
 
 export default {
-
+  data () {
+    return {
+      recommends: []
+    }
+  },
+  created () {
+    this._getRecommend()
+  },
+  methods: {
+    _getRecommend () {
+      getRecommend().then((res) => {
+        if (res.code === ERR_OK) {
+          console.log(res.data.slider)
+          this.recommends = res.data.slider
+        }
+      })
+    }
+  },
+  components: {
+    Slider
+  }
 }
 </script>
 
