9ee9608c760ce8f3a908a5e90ff36e1fe6a495b7
flyweiing <flyweiing@163.com>
2018-07-24 00:49:31 +0800

    dev-sever

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -20,24 +20,33 @@
 
 <script type="text/ecmascript-6">
 import Slider from 'base/slider/slider'
-import { getRecommend } from 'api/recommend'
+import { getRecommend, getDiscList } from 'api/recommend'
 import { ERR_OK } from 'api/config'
 
 export default {
   data () {
     return {
-      recommends: []
+      recommends: [],
+      discList: []
     }
   },
   created () {
     this._getRecommend()
+    this._getDiscList()
   },
   methods: {
     _getRecommend () {
       getRecommend().then((res) => {
         if (res.code === ERR_OK) {
-          console.log(res.data.slider)
           this.recommends = res.data.slider
+        }
+      })
+    },
+    _getDiscList () {
+      getDiscList().then((res) => {
+        if (res.code === ERR_OK) {
+          console.log(res.data.list)
+          this.discList = res.data.list
         }
       })
     }
