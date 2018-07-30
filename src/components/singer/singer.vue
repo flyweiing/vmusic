c56e3a91994e0c9f2e3f5fa4a28cfb8df58702fb
flyweiing <flyweiing@163.com>
2018-07-31 00:32:24 +0800

    add 获取歌手数据

--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -1,19 +1,88 @@
 <template>
-  <div>
-    <h1>{{ msg }}</h1>
+  <div class="singer" ref="singer">
   </div>
 </template>
+
 <script>
+import {getSingerList} from 'api/singer'
+import {ERR_OK} from 'api/config'
+import Singer from 'common/js/singer'
+
+const HOT_SINGER_LEN = 10
+const HOT_NAME = '热门'
+
 export default {
-  name: 'recommend',
   data () {
     return {
-      msg: 'singer'
+      singers: []
     }
+  },
+  created () {
+    this._getSingerList()
+  },
+  methods: {
+    _getSingerList () {
+      getSingerList().then((res) => {
+        if (res.code === ERR_OK) {
+          this.singers = this._normalizeSinger(res.data.list)
+          console.log(this.singers)
+        }
+      })
+    },
+    _normalizeSinger (list) {
+      let map = {
+        hot: {
+          title: HOT_NAME,
+          items: []
+        }
+      }
+      list.forEach((item, index) => {
+        if (index < HOT_SINGER_LEN) {
+          map.hot.items.push(new Singer({
+            name: item.Fsinger_name,
+            id: item.Fsinger_mid
+          }))
+        }
+        const key = item.Findex
+        if (!map[key]) {
+          map[key] = {
+            title: key,
+            items: []
+          }
+        }
+        map[key].items.push(new Singer({
+          name: item.Fsinger_name,
+          id: item.Fsinger_mid
+        }))
+      })
+      // 为了得到有序列表，我们需要处理 map
+      let ret = []
+      let hot = []
+      for (let key in map) {
+        let val = map[key]
+        if (val.title.match(/[a-zA-Z]/)) {
+          ret.push(val)
+        } else if (val.title === HOT_NAME) {
+          hot.push(val)
+        }
+      }
+      ret.sort((a, b) => {
+        return a.title.charCodeAt(0) - b.title.charCodeAt(0)
+      })
+      return hot.concat(ret)
+    }
+  },
+  components: {
+
   }
 }
+
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  .singer
+    position: fixed
+    top: 88px
+    bottom: 0
+    width: 100%
 </style>
